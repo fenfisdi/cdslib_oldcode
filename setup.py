b2eb3f08fdca7bf4de30988b307ba5a290f9c162
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-10-27 23:24:15 -0500

    refactor: setup.py conventional way (see python docs)

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+import setuptools 
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='cdslib',
     version='0.1.0',
     description='Contagious Disease Simulation - Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Camilo-HG/cdslib',
     author='Camilo Hincapié Gutiérrez',
     author_email='camilo.hincapie@udea.edu.co',
     license='gplv3',
-    packages=['cdslib'],
+    packages=setuptools.find_packages(),
     zip_safe=False
     )
